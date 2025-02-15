c392a29fae218ccd2e7e6b0f37b2a72d6f469729
Fahmeed Nabi <pmd4nd@virginia.edu>
2025-02-15 17:45:34 -0500

    Update url for login/public

--- music/urls.py
+++ music/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login_page, name='login'),
-    path("/anon", views.anonymous_front, name="anon")
+    path("login/", views.login_page, name='login'),
+    path("anon/", views.anonymous_front, name="anon")
 ]
