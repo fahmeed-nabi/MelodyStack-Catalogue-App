6c2a73dadcae52affd079dae741d2f84d6dbc37e
Ryan Nguyen <rnguyen513@gmail.com>
2025-02-15 17:34:09 -0500

    update anonymous_front

--- music/urls.py
+++ music/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.login_page, name='login'),
-    path("/anonymous", views.anonymous_front, name="anonymous_front")
+    path("/anon", views.anonymous_front, name="anon")
 ]
