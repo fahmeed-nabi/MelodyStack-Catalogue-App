59f7bdf363fafeca452b416f0aa8e3e9204ec129
Fahmeed Nabi <pmd4nd@virginia.edu>
2025-02-18 12:03:26 -0500

    Update models

--- music/urls.py
+++ music/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_page, name='login'),
-    path("anon/", views.anonymous_front.as_view(), name="anon")
+    path("public-collections/", views.anonymous_front.as_view(), name="anon")
     # path('librarian/<str:pk>/', views.librarian_page, name='librarian'),
     # path('patron/<str:pk>/', views.patron_page, name='patron'),
 ]
