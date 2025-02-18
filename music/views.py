db01944d79f4606986c8d2689511300d375875c2
Fahmeed Nabi <pmd4nd@virginia.edu>
2025-02-18 12:11:30 -0500

    Update models

--- music/views.py
+++ music/views.py
@@ -8,15 +8,15 @@ from .models import Item, Librarian, Patron
 def login_page(request):
     return render(request, "music/login_page.html")
 
-# def anonymous_front(request):
-#     return render(request, "music/anonymous_front.html")
+def anonymous_front(request):
+    return render(request, "music/anonymous_front.html")
 
-class anonymous_front(generic.ListView):
-    template_name = "music/anonymous_front.html"
-    context_object_name = "items" # TODO: implement Item model
-    
-    def get_queryset(self):
-        return Item.objects.all() # TODO: implement Item model
+# class anonymous_front(generic.ListView):
+#     template_name = "music/anonymous_front.html"
+#     context_object_name = "items" # TODO: implement Item model
+#
+#     def get_queryset(self):
+#         return Item.objects.all() # TODO: implement Item model
 
 def librarian_page(request, user_id):
     librarian = get_object_or_404(Librarian, pk=user_id)
