76812cb15d0174d71a743a9f6902a45887cacb2a
arjkrao <arjun.krishna.rao@gmail.com>
2025-02-20 12:43:05 -0500

    Added functionality for logging in as patron and librarian

--- music/views.py
+++ music/views.py
@@ -2,11 +2,44 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views import generic
+from django.contrib.auth import get_user, logout
+from datetime import datetime
 
 from .models import Item, Librarian, Patron
 
 def login_page(request):
-    return render(request, "music/login_page.html")
+    curr_user = get_user(request)
+    if(curr_user.is_anonymous):
+        return render(request, "music/login_page.html")
+    
+    librarians = Librarian.objects.filter(user=curr_user)
+    patrons = Patron.objects.filter(user=curr_user)
+    
+    if(librarians.exists()):
+        return redirect("librarian")
+    elif(patrons.exists()):
+        return redirect("patron")
+    else:
+        return render(request, "music/login_page.html")
+
+def redir(request):
+    curr_user = get_user(request)
+    librarians = Librarian.objects.filter(user=curr_user)
+    patrons = Patron.objects.filter(user=curr_user)
+    
+    if(librarians.exists()):
+        return redirect("librarian")
+    elif(patrons.exists()):
+        return redirect("patron")
+    else:
+        new_patron = Patron.objects.create(
+            user = curr_user,
+            name = curr_user.email,
+            google_account = curr_user.email,
+            date_joined = datetime.now()
+        )
+        return redirect("patron")
+       
 
 def anonymous_front(request):
     return render(request, "music/anonymous_front.html")
@@ -18,14 +51,20 @@ def anonymous_front(request):
 #     def get_queryset(self):
 #         return Item.objects.all() # TODO: implement Item model
 
-def librarian_page(request, user_id):
-    librarian = get_object_or_404(Librarian, pk=user_id)
+def librarian_page(request):
+    curr_user = get_user(request)
+    librarian = Librarian.objects.filter(user=curr_user).first()
     return render(request, "music/librarian.html", {
-        'librarian' : librarian
+        'librarian' : librarian.user.email
     })
 
-def patron_page(request, user_id):
-    patron = get_object_or_404(Patron, pk=user_id)
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def patron_page(request):
+    curr_user = get_user(request)
+    patron = Patron.objects.filter(user=curr_user).first()
     return render(request, "music/patron.html", {
-        'patron' : patron
+        'patron' : patron.user.email
     })
