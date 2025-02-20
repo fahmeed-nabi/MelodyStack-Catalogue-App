66b68863f3c39ca54f77a7e2945a5f8241301e78
arjkrao <arjun.krishna.rao@gmail.com>
2025-02-20 12:42:36 -0500

    Added URLs to add a new patron if a new account is detected or send it to the page that it was already designated, added URL to log out

--- music/urls.py
+++ music/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("login/", views.login_page, name='login'),
-    path("public-collections/", views.anonymous_front, name="anon")
-    # path('librarian/<str:pk>/', views.librarian_page, name='librarian'),
-    # path('patron/<str:pk>/', views.patron_page, name='patron'),
+    path("public-collections/", views.anonymous_front, name="anon"),
+    path('librarian/', views.librarian_page, name='librarian'),
+    path('patron/', views.patron_page, name='patron'),
+    path('redir', views.redir, name="redir"),
+    path("logout", views.logout_view, name="logout_view"),
 ]
