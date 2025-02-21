74c3ee299bcbef8582c0508333fb6f7a76bc46d8
Ryan Nguyen <rnguyen513@gmail.com>
2025-02-21 15:44:14 -0500

    add collection filters to anonymous music media storefront

--- music/admin.py
+++ music/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item
+from . import models
 
 # Register your models here.
-admin.site.register(Item)
+admin.site.register([model for model in models.__dict__.values() if isinstance(model, type)])
--- music/views.py
+++ music/views.py
@@ -3,13 +3,22 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views import generic
 
-from .models import Item, Librarian, Patron
+from .models import Item, Librarian, Patron, Collection
 
 def login_page(request):
     return render(request, "music/login_page.html")
 
 def anonymous_front(request):
-    return render(request, "music/anonymous_front.html")
+    collections = Collection.objects.all()
+    selected_collection_id = request.GET.get("collection")
+
+    if selected_collection_id:
+        selected_collection = Collection.objects.get(id=selected_collection_id)
+        items = selected_collection.items.all()
+    else:
+        items = Item.objects.all()
+
+    return render(request, "music/anonymous_front.html", {"items":items, "collections":collections})
 
 # class anonymous_front(generic.ListView):
 #     template_name = "music/anonymous_front.html"
