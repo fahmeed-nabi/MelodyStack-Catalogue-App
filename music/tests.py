fe1f895c1fef3bae345d652de9c3e5e94530a0b3
arjkrao <arjun.krishna.rao@gmail.com>
2025-03-12 19:59:29 -0400

    Added basic tests to make sure first few pages are accessible

--- music/tests.py
+++ music/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class RenderLoginPage(TestCase):
+    def test_render_login_page(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+class RenderingAnonymousFrontPage(TestCase):
+    def test_render_anonymous_front_page(self):
+        response = self.client.get(reverse("public"))
+        self.assertEqual(response.status_code, 200)
