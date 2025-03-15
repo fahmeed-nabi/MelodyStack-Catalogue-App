a144d7a5655dd4fb49e0c38a81afe189cf0346da
arjkrao <arjun.krishna.rao@gmail.com>
2025-03-15 17:02:51 -0400

    Added setup for testing user accounts

--- music/tests.py
+++ music/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your tests here.
 
@@ -7,8 +10,28 @@ class RenderLoginPage(TestCase):
     def test_render_login_page(self):
         response = self.client.get(reverse("login"))
         self.assertEqual(response.status_code, 200)
-
 class RenderingAnonymousFrontPage(TestCase):
     def test_render_anonymous_front_page(self):
         response = self.client.get(reverse("public"))
         self.assertEqual(response.status_code, 200)
+
+class TestPatronRendering(TestCase):
+    def setUp(self):
+        username = "testinguser"
+        first_name = "Testing"
+        last_name = "User"
+        email_address = "test@example.com"
+        
+        self.user = User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email_address
+            )
+
+    def test_patron_rendering(self):
+        print("Testing user name: ", self.user.username)
+        self.assertEqual(self.user.username, "testinguser")
+
+
+    
