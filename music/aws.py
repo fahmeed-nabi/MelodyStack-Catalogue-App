0b0d4ec00a06334c89a0b1c86e57cbade96f77f5
arjkrao <arjun.krishna.rao@gmail.com>
2025-03-15 23:57:11 -0400

    Makes sure .env isn't loaded during GitHub Actions

--- music/aws.py
+++ music/aws.py
@@ -1,7 +1,9 @@
 import os, boto3
 from dotenv import load_dotenv
 from botocore.client import Config
-load_dotenv("env")
+
+if(os.environ.get("GITHUB_ACTIONS") != True):
+    load_dotenv("env")
 
 def upload_file(filename, bucket, object_name=None):
     '''
