16230dfcda98e1acfa88a3ef1377575e1301fe33
arjkrao <arjun.krishna.rao@gmail.com>
2025-03-29 12:52:42 -0400

    Added paths for incoming and outgoing requests, including approving and denying requests for librarians

--- music/urls.py
+++ music/urls.py
@@ -12,10 +12,14 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout_view"),
     path('patron/settings/', views.patron_settings_view, name='patron_settings'),
     path('librarian/settings/', views.librarian_settings_view, name='librarian_settings'),
+    path('librarian/incoming_requests', views.incoming_requests, name='incoming_requests'),
     path('librarian/create_collection_item/', views.create_collection_item, name='create_collection_item'),
     path('librarian/manage_collections/', views.manage_collections, name='manage_collections'),
     path('librarian/manage_collections/edit_collection/<slug:title>/<int:collection_id>/', views.edit_collection, name='edit_collection'),
     path('librarian/manage_collections/delete_collection/<slug:title>/<int:collection_id>/', views.delete_collection, name='delete_collection'),
+    path('librarian/incoming_requests/approve_request/<int:borrow_request_id>', views.approve_request, name="approve_request"),
+    path('librarian/incoming_requests/deny_request/<int:borrow_request_id>', views.deny_request, name="deny_request"),
+    path('librarian/outgoing_requests', views.outgoing_requests, name="outgoing_requests"),
     path('item/<int:pk>/edit/', views.ItemEditView.as_view(), name='item_edit'),
     path('item/<int:pk>/delete/', views.ItemDeleteView.as_view(), name='item_delete'),
     path('borrow_redir/<int:pk>/', views.borrow_redir, name='borrow_redirect'),
@@ -27,6 +31,7 @@ urlpatterns = [
     path('patron/manage_collections/edit_collection/<slug:title>/<int:collection_id>/', views.edit_collection_patron, name='edit_collection_patron'),
     path('patron/manage_collections/delete_collection/<slug:title>/<int:collection_id>/', views.delete_collection_patron,
          name='delete_collection_patron'),
+    path('patron/outgoing_requests', views.outgoing_requests, name='outgoing_requests'),
 
 ]
 
