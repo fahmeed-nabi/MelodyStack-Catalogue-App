3cff45941606f136147fd0c2935eaa43fa9d2ca1
Fahmeed Nabi <pmd4nd@virginia.edu>
2025-04-09 22:10:23 -0400

    update views structure

--- music/views/__init__.py
+++ music/views/__init__.py
@@ -2,6 +2,6 @@
 
 from .auth_views import *
 from .collection_views import *
-from item_views import *
-from librarian_views import *
-from patron_views import *
+from .item_views import *
+from .librarian_views import *
+from .patron_views import *
