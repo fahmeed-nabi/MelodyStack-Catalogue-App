2254845c2473d6ba3bdc6516267b9455687647ed
arjkrao <arjun.krishna.rao@gmail.com>
2025-04-22 11:45:04 -0400

    Added serial number field to items and made it display in item detail view

--- music/models.py
+++ music/models.py
@@ -22,7 +22,7 @@ class Item(models.Model):
     ]
 
     id = models.AutoField(primary_key=True)
-    unique_str = models.UUIDField(default=uuid.uuid4, blank=False, null=False, unique=True)
+    serial_num = models.UUIDField(default=uuid.uuid4, blank=False, null=False, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     status = models.CharField(
