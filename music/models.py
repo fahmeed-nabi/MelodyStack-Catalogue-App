60abff46ec295a3094cc8308dbb2063d266b8ad4
arjkrao <arjun.krishna.rao@gmail.com>
2025-04-22 11:32:20 -0400

    potentially implemented unique id for items

--- music/models.py
+++ music/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.utils.text import slugify
 from . import aws
 from mysite.settings import os.environ.get('BUCKET_NAME')
-
+import uuid
 User = get_user_model()
 
 # Create your models here.
@@ -22,6 +22,7 @@ class Item(models.Model):
     ]
 
     id = models.AutoField(primary_key=True)
+    unique_str = models.UUIDField(default=uuid.uuid4, blank=False, null=False, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=500, blank=True, null=True)
     status = models.CharField(
